6c84d4fc55db58fe00e49567957490f47ebfcb01
Shubham <81020379+svvetal@users.noreply.github.com>
2022-04-24 23:07:48 +0530

    Add files via upload

--- Motor_Final.py
+++ Motor_Final.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import RPi.GPIO as GPIO
 from time import sleep
 
@@ -29,6 +29,14 @@ GPIO.setup(ir_center, GPIO.IN)
 GPIO.setup(ir_right, GPIO.IN)
 GPIO.setup(buzz, GPIO.OUT)
 GPIO.output(buzz,0)
+
+servo = 26
+
+GPIO.setup(servo, GPIO.OUT)
+
+p = GPIO.PWM(servo, 50)
+
+p.start(0)
 
 
 app = Flask(__name__, template_folder='template')
@@ -77,26 +85,41 @@ def stop():
     GPIO.output(mB2, 0)
     return ("nothing")
 
+@app.route('/set_speed')
+def set_speed():
+    speed = request.args.get("speed")
+    p.ChangeDutyCycle(int(speed))
+    return render_template('Motor_Final.html')
+
 @app.route('/update')
 def update():
     data_1 = GPIO.input(ir_left)
     data_2 = GPIO.input(ir_center)
     data_3 = GPIO.input(ir_right)
+    global irl,irc,irr
+    irl = "No Obstacle Detected"
+    irc = "No Obstacle Detected"
+    irr = "No Obstacle Detected"
     if data_1 == 0:
+        irl = "Obstacle Detected"
         GPIO.output(buzz, 1)
     elif data_2 == 0:
+        irc = "Obstacle Detected"
         GPIO.output(buzz, 1)
     elif data_3 == 0:
+        irr = "Obstacle Detected"
         GPIO.output(buzz, 1)
     elif data_1 == 1:
+        irl = "No Obstacle Detected"
         GPIO.output(buzz, 0)
     elif data_2 == 1:
+        irc = "No Obstacle Detected"
         GPIO.output(buzz, 0)
     elif data_3 == 1:
+        irr = "No Obstacle Detected"
         GPIO.output(buzz, 0)
-    templateData = {'data1' : data_1, 'data2' : data_2, 'data3' : data_3}
+    templateData = {'data1' : irl, 'data2' : irc, 'data3' : irr}
     return jsonify(templateData), 200
-        
 
 if __name__ == '__main__':
     app.run()
