cce0df7a9cc9bcad8b04f8aef0ad8253095180e0
Shubham <81020379+svvetal@users.noreply.github.com>
2022-04-28 04:56:04 +0100

    final files op

--- app.py
+++ app.py
@@ -6,6 +6,54 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 
+import flask
+from flask import Flask, render_template, jsonify, request
+import RPi.GPIO as GPIO
+from time import sleep
+
+from flask import Flask, render_template, Response
+import os
+
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
+
+mA1=18
+mA2=23
+mB1=24
+mB2=25
+
+ir_left = 2
+ir_center = 3
+ir_right = 4
+buzz = 17
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(mA1, GPIO.OUT)
+GPIO.setup(mA2, GPIO.OUT)
+GPIO.setup(mB1, GPIO.OUT)
+GPIO.setup(mB2, GPIO.OUT)
+GPIO.output(mA1 , 0)
+GPIO.output(mA2 , 0)
+GPIO.output(mB1, 0)
+GPIO.output(mB2, 0)
+
+GPIO.setup(ir_left, GPIO.IN)
+GPIO.setup(ir_center, GPIO.IN)
+GPIO.setup(ir_right, GPIO.IN)
+GPIO.setup(buzz, GPIO.OUT)
+GPIO.output(buzz,0)
+
+servo = 26
+
+GPIO.setup(servo, GPIO.OUT)
+
+p = GPIO.PWM(servo, 50)
+
+p.start(0)
+
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -100,5 +148,92 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/forward')
+def forward():
+    GPIO.output(mA1 , 1)
+    GPIO.output(mA2 , 0)
+    GPIO.output(mB1, 0)
+    GPIO.output(mB2, 1)
+    return ("nothing")
+
+@app.route('/backward')
+def backward():
+    GPIO.output(mA1 , 0)
+    GPIO.output(mA2 , 1)
+    GPIO.output(mB1, 1)
+    GPIO.output(mB2, 0)
+    return ("nothing")
+
+@app.route('/right')
+def right():
+    GPIO.output(mA1 , 0)
+    GPIO.output(mA2 , 1)
+    GPIO.output(mB1, 0)
+    GPIO.output(mB2, 0)
+    return ("nothing")
+
+@app.route('/left')
+def left():
+    GPIO.output(mA1 , 0)
+    GPIO.output(mA2 , 0)
+    GPIO.output(mB1, 1)
+    GPIO.output(mB2, 0)
+    return ("nothing")
+
+@app.route('/stop')
+def stop():
+    GPIO.output(mA1 , 0)
+    GPIO.output(mA2 , 0)
+    GPIO.output(mB1, 0)
+    GPIO.output(mB2, 0)
+    return ("nothing")
+
+@app.route('/set_speed')
+def set_speed():
+    speed = request.args.get("speed")
+    p.ChangeDutyCycle(int(speed))
+    return render_template('Motor_test.html')
+
+@app.route('/update')
+def update():
+    data_1 = GPIO.input(ir_left)
+    data_2 = GPIO.input(ir_center)
+    data_3 = GPIO.input(ir_right)
+    global irl,irc,irr
+    irl = "No Obstacle Detected"
+    irc = "No Obstacle Detected"
+    irr = "No Obstacle Detected"
+    if data_1 == 0:
+        irl = "Obstacle Detected"
+        GPIO.output(buzz, 1)
+    elif data_2 == 0:
+        irc = "Obstacle Detected"
+        GPIO.output(buzz, 1)
+    elif data_3 == 0:
+        irr = "Obstacle Detected"
+        GPIO.output(buzz, 1)
+    elif data_1 == 1:
+        irl = "No Obstacle Detected"
+        GPIO.output(buzz, 0)
+    elif data_2 == 1:
+        irc = "No Obstacle Detected"
+        GPIO.output(buzz, 0)
+    elif data_3 == 1:
+        irr = "No Obstacle Detected"
+        GPIO.output(buzz, 0)
+    templateData = {'data1' : irl, 'data2' : irc, 'data3' : irr}
+    return jsonify(templateData), 200
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
